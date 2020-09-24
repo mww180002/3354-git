819a5f66db5cc37e469ebd43e2057f2237a4416a
Malikye Wallace <mww180002@utdallas.edu>
2020-09-24 00:28:53 -0500

    The adder program file has been modified to add multiple integers given by space separated input from the command line.

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -1,4 +1,5 @@
 package adder;
+import java.util.*;
 
 public class Main {
 
@@ -7,11 +8,22 @@ public class Main {
             int result = addArguments(args);
             System.out.println(result);
         } catch (Exception e) {
-            System.err.println("Please provide two integers to add");
+            System.err.println("Please provide some integers to add.");
         }
     }
 
     private static int addArguments(String[] args) {
-        return Integer.valueOf(args[0]) + Integer.valueOf(args[1]);
+        String input = "";
+        int total = 0;
+        for (String str : args){
+            input = input + str + " ";
+        }
+        Scanner scanner = new Scanner(input);
+        while (scanner.hasNext()){
+            int n = scanner.nextInt();
+            total += n;
+        }
+
+        return total;
     }
 }
